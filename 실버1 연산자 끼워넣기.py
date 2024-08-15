caf8a98bda28e2b0977db0971770242a74fd7ade
jmc98 <jmc0820@naver.com>
2024-08-15 19:17:39 +0900

    24.08.11 - 시간 초과, 다른 사람 풀이 참고
    시간 줄인 버전.

--- 실버1 연산자 끼워넣기.py
+++ 실버1 연산자 끼워넣기.py
@@ -1,34 +1,31 @@
 # https://www.acmicpc.net/problem/14888
-def cal(order):
-    result = seq[0]
-    for i in range(1, n):  # order의 숫자에 따라 계산을 함
-        if order[i - 1] == "0":
-            result += seq[i]
-        elif order[i - 1] == "1":
-            result -= seq[i]
-        elif order[i - 1] == "2":
-            result *= seq[i]
-        else:
-            if result > 0:
-                result //= seq[i]
-            else:  # 음수의 절대값을 나눈 다음 - 부호를 붙임.
-                result = (abs(result) // seq[i]) * -1
-    return result
+def cal(op, num, depth):    # 계산하기
+    if op == 0:
+        num += seq[depth]
+    elif op == 1:
+        num -= seq[depth]
+    elif op == 2:
+        num *= seq[depth]
+    else:
+        if num > 0:
+            num //= seq[depth]
+        else:  # 음수의 절대값을 나눈 다음 - 부호를 붙임.
+            num = (abs(num) // seq[depth]) * -1
+    return num
 
 
-def dfs(depth, order):
+def dfs(depth, result):
     global maxResult
     global minResult
     global opNum
-    if depth == 0:
-        result = cal(order)  # 연산에 따라 계산
+    if depth == n:
         maxResult = max(maxResult, result)
         minResult = min(minResult, result)
     else:
         for i in range(4):
             if opNum[i] > 0:    # 연산자 티오가 있으면
                 opNum[i] -= 1   # 티오를 하나 줄이고 재귀 호출
-                dfs(depth - 1, order + str(i))
+                dfs(depth + 1, cal(i, result, depth))
                 opNum[i] += 1   # 티오 복구
 
 
@@ -37,6 +34,6 @@ seq = list(map(int, input().split()))
 opNum = list(map(int, input().split()))  # 여러 숫자 리스트로 입력 받기
 maxResult = -1000000000
 minResult = 1000000000
-dfs(n - 1, "")
+dfs(1, seq[0])
 print(maxResult)
 print(minResult)
