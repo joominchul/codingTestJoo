4fb2210435fabcbf92dba38228fc66383d0cd414
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2025-01-14 16:33:51 +0900

    25.01.14

--- Lv.2 예상 대진표.py
+++ Lv.2 예상 대진표.py
@@ -1,1 +1,13 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12985
+def solution(n,a,b):
+    answer = 0
+    while(a != b):
+        if a % 2 != 0:
+            a += 1
+        if b % 2 != 0:
+            b += 1
+        a = a/2
+        b = b/2
+        answer += 1
 
+    return answer
