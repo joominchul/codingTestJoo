6a6de78626ae9fd547edb413b348a48f86eebe61
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2025-01-26 20:58:50 +0900

    25.01.26

---
+++ 코틀린/Lv.0 숫자 비교하기.kt
@@ -0,0 +1,10 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120807
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = -1
+        if(num1 == num2){
+            return 1
+        }
+        return answer
+    }
+}
