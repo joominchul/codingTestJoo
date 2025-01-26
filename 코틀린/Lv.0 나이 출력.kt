5f215526a090b3cd8e170cb0d6c2f90e719be649
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2025-01-26 21:01:28 +0900

    25.01.26

---
+++ 코틀린/Lv.0 나이 출력.kt
@@ -0,0 +1,7 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/120820
+class Solution {
+    fun solution(age: Int): Int {
+        var answer: Int = 2022 - age + 1 
+        return answer
+    }
+}
